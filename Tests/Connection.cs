f966b1c94771647433c21f46c9acee5ae0bfa62f
Simon Cropp <simon.cropp@gmail.com>
2018-01-24 19:34:28 +1100

    add appveyor stuff

--- Tests/Connection.cs
+++ Tests/Connection.cs
@@ -1,8 +1,19 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data.SqlClient;
 
 public static class Connection
 {
-    public const string ConnectionString = @"Data Source=.\SQLExpress;Database=NServiceBusAttachmentsTests; Integrated Security=True;Max Pool Size=100";
+    public static string ConnectionString;
+
+    static Connection()
+    {
+        if (Environment.GetEnvironmentVariable("APPVEYOR")=="True")
+        {
+            ConnectionString = @"Server=(local)\SQL2017;Database=master;User ID=sa;Password=Password12!";
+            return;
+        }
+        ConnectionString = @"Data Source=.\SQLExpress;Database=NServiceBusAttachmentsTests; Integrated Security=True;Max Pool Size=100";
+    }
 
     public static SqlConnection OpenConnection()
     {
