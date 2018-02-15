f82fd6f295e0662e9043710194c1722ab421a6ca
Simon Cropp <simon.cropp@gmail.com>
2018-02-15 12:56:27 +1100

    cleanup

--- NServiceBus.Attachments.Sql - Copy/Cleanup/IAsyncTimer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-
-interface IAsyncTimer
-{
-    void Start(Func<DateTime, CancellationToken, Task> callback, TimeSpan interval, Action<Exception> errorCallback, Func<TimeSpan, CancellationToken, Task> delayStrategy);
-    Task Stop();
-}
