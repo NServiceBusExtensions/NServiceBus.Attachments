1e3038ecb40ace3be9398aeb843f7fed532ad59b
Simon Cropp <simon.cropp@gmail.com>
2018-09-26 11:03:49 +1000

    .

--- Attachments.Sql.Tests/IntegrationTests/IntegrationTests.cs
+++ Attachments.Sql.Tests/IntegrationTests/IntegrationTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
@@ -27,70 +26,6 @@ public class IntegrationTests
     public IntegrationTests()
     {
         shouldPerformNestedConnection = true;
-    }
-    public class TestDataGenerator : IEnumerable<object[]>
-    {
-        List<TransportTransactionMode> transactionModes = new List<TransportTransactionMode>
-        {
-            TransportTransactionMode.None,
-            TransportTransactionMode.ReceiveOnly,
-            TransportTransactionMode.SendsAtomicWithReceive,
-            TransportTransactionMode.TransactionScope,
-        };
-        List<bool> useSqlPersistenceList = new List<bool>
-        {
-            true,
-            false
-        };
-        List<bool> useSqlTransportList = new List<bool>
-        {
-            true,
-            false
-        };
-        List<bool> useStorageSessionList = new List<bool>
-        {
-            true,
-            false
-        };
-        List<bool> useSqlTransportConnectionList = new List<bool>
-        {
-            true,
-            false
-        };
-
-        public IEnumerator<object[]> GetEnumerator()
-        {
-            foreach (var useSqlPersistence in useSqlPersistenceList)
-            {
-                foreach (var useSqlTransportConnection in useSqlTransportConnectionList)
-                {
-                    foreach (var useStorageSession in useStorageSessionList)
-                    {
-                        foreach (var useSqlTransport in useSqlTransportList)
-                        {
-                            foreach (var mode in transactionModes)
-                            {
-                                if (!useSqlTransport && mode == TransportTransactionMode.TransactionScope)
-                                {
-                                    continue;
-                                }
-
-                                yield return new object[]
-                                {
-                                    useSqlTransport,
-                                    useSqlTransportConnection,
-                                    useSqlPersistence,
-                                    useStorageSession,
-                                    mode
-                                };
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 
     //[Fact]
