f22c1e403edeae808e8cb55a2dcbb9d6ae436fa0
Simon Cropp <simon.cropp@gmail.com>
2019-08-12 11:58:29 +1000

    add CopyToDirectory

---
+++ src/Shared/Incoming/IncomingAttachmentExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NServiceBus.Attachments
+#if FileShare
+    .FileShare
+#endif
+#if Sql
+    .Sql
+#endif
+#if Raw
+    .Raw
+#endif
+{
+    /// <summary>
+    /// Extensions for <see cref="IMessageAttachments"/>.
+    /// </summary>
+    public static class IncomingAttachmentExtensions
+    {
+        /// <summary>
+        /// Copies all attachments for the current message to <paramref name="directory"/>.
+        /// </summary>
+        public static Task CopyToDirectory(
+            this IMessageAttachments attachments,
+            string directory,
+            string nameForDefault = default,
+            CancellationToken cancellation = default)
+        {
+            Guard.AgainstNullOrEmpty(directory, nameof(directory));
+            Guard.AgainstEmpty(nameForDefault, nameof(nameForDefault));
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            return attachments.ProcessStreams(
+                async (name, stream) =>
+                {
+                    if (name == "default" && nameForDefault != null)
+                    {
+                        name = nameForDefault;
+                    }
+
+                    using (var fileStream = File.Create(Path.Combine(directory, name)))
+                    {
+                        await stream.CopyToAsync(fileStream, 4096, cancellation);
+                    }
+                },
+                cancellation);
+        }
+    }
+}
