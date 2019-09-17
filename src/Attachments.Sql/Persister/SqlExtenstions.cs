1e8bd799e9599717649e3a506750ab46cd9bfb20
Simon Cropp <simon.cropp@gmail.com>
2019-09-17 20:01:46 +1000

    Update SqlExtenstions.cs

--- src/Attachments.Sql/Persister/SqlExtenstions.cs
+++ src/Attachments.Sql/Persister/SqlExtenstions.cs
@@ -16,22 +16,30 @@ static class SqlExtensions
         return dataReader.GetString(index);
     }
 
-    public static void AddParameter(this SqlCommand command, string name, string value)
+    public static void AddParameter(this IDbCommand command, string name, string value)
     {
-        var parameter = command.Parameters.Add(name, SqlDbType.NVarChar);
-        if (value != null)
-        {
-            parameter.Value = value;
-        }
-        else
+        var parameter = command.CreateParameter();
+        parameter.DbType = DbType.String;
+        parameter.ParameterName = name;
+        if (value == null)
         {
             parameter.Value = DBNull.Value;
         }
+        else
+        {
+            parameter.Value = value;
+        }
+
+        command.Parameters.Add(parameter);
     }
 
-    public static void AddBinary(this SqlCommand command, string name, object value)
+    public static void AddBinary(this IDbCommand command, string name, object value)
     {
-        command.Parameters.Add(name, SqlDbType.Binary, -1).Value = value;
+        var parameter = command.CreateParameter();
+        parameter.ParameterName = name;
+        parameter.DbType = DbType.Binary;
+        parameter.Value = value;
+        command.Parameters.Add(parameter);
     }
 
     public static void AddParameter(this SqlCommand command, string name, DateTime value)
