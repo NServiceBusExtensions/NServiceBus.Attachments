19dd6767d3adbfaf1261c27b45740568948bed81
Simon Cropp <simon.cropp@gmail.com>
2024-04-18 16:09:11 +1000

    Update IncomingWhenNotEnabledTests.cs

--- src/Attachments.FileShare.Tests/WhenNotEnabled/IncomingWhenNotEnabledTests.cs
+++ src/Attachments.FileShare.Tests/WhenNotEnabled/IncomingWhenNotEnabledTests.cs
@@ -10,6 +10,9 @@
         configuration.UsePersistence<LearningPersistence>();
         configuration.RegisterComponents(_ => _.AddSingleton(this));
         configuration.UseTransport<LearningTransport>();
+        configuration.AssemblyScanner()
+            .ExcludeAssemblies("xunit.runner.utility.netcoreapp10.dll");
+        configuration.UseSerialization<SystemJsonSerializer>();
         var endpoint = await Endpoint.Start(configuration);
         await endpoint.SendLocal(new SendMessage());
         ResetEvent.WaitOne();
