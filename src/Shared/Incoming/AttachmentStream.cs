02babafff6065fb5fa50c06380b559ecee8624cf
Simon Cropp <simon.cropp@gmail.com>
2025-07-26 19:08:08 +1000

    Update AttachmentStream.cs

--- src/Shared/Incoming/AttachmentStream.cs
+++ src/Shared/Incoming/AttachmentStream.cs
@@ -1,4 +1,5 @@
-﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+﻿// ReSharper disable CollectionNeverUpdated.Local
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 namespace NServiceBus.Attachments
 #if FileShare
 .FileShare
